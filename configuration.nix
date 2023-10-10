9578f444d0fc775680276ced0c88d1094b420753
Kaidenleaf <129576524+Kaidenleaf@users.noreply.github.com>
2023-10-09 22:05:07 -0600

    add: hyprland

--- configuration.nix
+++ configuration.nix
@@ -118,6 +118,13 @@
     google-chrome
     jdk17
   ];
+  
+  fonts.packages = with pkgs; [
+    noto-fonts-cjk
+    (nerdfonts.override { fonts = [ "JetBrainsMono" ]; })
+  ];
+
+  programs.hyprland.enable = true;
 
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
